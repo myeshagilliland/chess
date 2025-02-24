498188454a8fb5dc3b6ddccbb110a23625fddea6
myeshagilliland <myeshagilliland@gmail.com>
2025-02-24 14:29:25 -0700

    User DAOs implemented

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,14 +1,31 @@
 package dataaccess;
 
 //import java.util.Map;
+import model.UserData;
+
 import java.util.HashMap;
 
 public class MemoryUserDAO implements UserDAO {
-    // username: [password, email]
-    private HashMap<String, String> userData = new HashMap<String, String>();
+    // username: userData (username, password, email)
+    private HashMap<String, UserData> userDatabase = new HashMap<String, UserData>();
 
     @Override
-    void public create(HashMap<String, String> userData) {
+    public void create(UserData userData) {
+        userDatabase.put(userData.username(), userData);
+    }
 
+    @Override
+    public UserData read(String username) {
+        return userDatabase.get(username);
+    }
+
+    @Override
+    public void update(UserData userData) {
+        userDatabase.replace(userData.username(), userData);
+    }
+
+    @Override
+    public void delete(String username) {
+        userDatabase.remove(username);
     }
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,8 +1,19 @@
 package dataaccess;
 
-public interface UserDAO<username, userData> {
-    void create(userData info);
-    userData read(username user);
-    void update(userData info);
-    void delete(username user);
+import model.UserData;
+
+public interface UserDAO {
+//    <username, userData>
+//    void create(userData info);
+//    userData read(username user);
+//    void update(userData info);
+//    void delete(username user);
+
+    void create(UserData userData);
+
+    UserData read(String username);
+
+    void update(UserData userData);
+
+    void delete(String username);
 }
