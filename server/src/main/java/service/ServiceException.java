82d662680fbc6af228344d70e357d2e8cfb31b0b
myeshagilliland <myeshagilliland@gmail.com>
2025-03-08 15:52:15 -0700

    added new exception classes

---
+++ server/src/main/java/service/ServiceException.java
@@ -0,0 +1,18 @@
+package service;
+
+/**
+ * Indicates there was an error at the service level
+ */
+public class ServiceException extends Exception{
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public String getErrorMessage () {
+        return "{\"message\": \"Error: "+getMessage()+"\"}";
+    }
+
+    public int getStatusCode () {
+        return 500;
+    }
+}
