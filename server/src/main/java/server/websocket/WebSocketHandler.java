d84d62dc188635dfb57025ea2cbff7d06988fa37
myeshagilliland <myeshagilliland@gmail.com>
2025-04-07 11:43:41 -0600

    Normal leave passing

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,5 +1,6 @@
 package server.websocket;
 
+import chess.ChessGame;
 import com.google.gson.Gson;
 import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
@@ -16,6 +17,7 @@ import websocket.messages.LoadGameMessage;
 import websocket.messages.NotificationMessage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 import static websocket.messages.ServerMessage.ServerMessageType.*;
@@ -27,6 +29,7 @@ public class WebSocketHandler {
 //    private final GameService gameService;
     private final AuthDAO authDAO;
     private final GameDAO gameDAO;
+    private ArrayList<Integer> inactiveGames = new ArrayList<>();
 
     public WebSocketHandler(AuthDAO authDAO, GameDAO gameDAO) {
         this.authDAO = authDAO;
@@ -39,7 +42,7 @@ public class WebSocketHandler {
         switch (command.getCommandType()) {
             case CONNECT -> connect(command.getAuthToken(), command.getGameID(), session);
 //            case MAKE_MOVE -> makeMove(action.visitorName());
-//            case LEAVE -> leave(action.visitorName());
+            case LEAVE -> leave(command.getAuthToken(), command.getGameID(), session);
 //            case RESIGN -> resign(action.visitorName());
         }
     }
@@ -85,6 +88,98 @@ public class WebSocketHandler {
 
         connections.sendLoadGameRoot(session, loadGame);
         connections.sendNotification(gameID, authData.username(), notification);
+    }
+
+    private void leave(String authToken, int gameID, Session session) throws IOException {
+
+        AuthData authData = null;
+        try {
+            authData = authDAO.findAuth(authToken);
+        } catch (DataAccessException e) {
+            connections.sendError(session, new ErrorMessage(ERROR, e.getMessage()));
+        }
+        if (authData == null) {
+            ErrorMessage errorMessage = new ErrorMessage(ERROR, "Error: Not logged in. Please try again");
+            connections.sendError(session, errorMessage);
+            return;
+        }
+
+        GameData gameData = null;
+        try {
+            gameData = gameDAO.findGame(gameID);
+        } catch (DataAccessException e) {
+            connections.sendError(session, new ErrorMessage(ERROR, e.getMessage()));
+        }
+        if (gameData == null) {
+            ErrorMessage errorMessage = new ErrorMessage(ERROR, "Error: Game not found. Please try again");
+            connections.sendError(session, errorMessage);
+            return;
+        }
+
+        GameData updatedGameData = null;
+        if (Objects.equals(authData.username(), gameData.whiteUsername())) {
+            updatedGameData = new GameData(gameData.gameID(), null, gameData.blackUsername(), gameData.gameName(), gameData.chessGame());
+        } else if (Objects.equals(authData.username(), gameData.blackUsername())) {
+            updatedGameData = new GameData(gameData.gameID(), gameData.whiteUsername(), null, gameData.gameName(), gameData.chessGame());
+        }
+
+        if (updatedGameData != null) {
+            try {
+                gameDAO.updateGame(updatedGameData);
+            } catch (DataAccessException e) {
+                ErrorMessage errorMessage = new ErrorMessage(ERROR, "Error: user not in game");
+                connections.sendError(session, errorMessage);
+                return;
+            }
+        }
+
+        connections.remove(gameID, authData.username());
+
+        var message = String.format("%s has left the game", authData.username());
+        var notification = new NotificationMessage(NOTIFICATION, message);
+        connections.sendNotification(gameID, authData.username(), notification);
+    }
+
+    private void resign(String authToken, int gameID, Session session) throws IOException {
+
+        AuthData authData = null;
+        try {
+            authData = authDAO.findAuth(authToken);
+        } catch (DataAccessException e) {
+            connections.sendError(session, new ErrorMessage(ERROR, e.getMessage()));
+        }
+        if (authData == null) {
+            ErrorMessage errorMessage = new ErrorMessage(ERROR, "Error: Not logged in. Please try again");
+            connections.sendError(session, errorMessage);
+            return;
+        }
+
+        GameData gameData = null;
+        try {
+            gameData = gameDAO.findGame(gameID);
+        } catch (DataAccessException e) {
+            connections.sendError(session, new ErrorMessage(ERROR, e.getMessage()));
+        }
+        if (gameData == null) {
+            ErrorMessage errorMessage = new ErrorMessage(ERROR, "Error: Game not found. Please try again");
+            connections.sendError(session, errorMessage);
+            return;
+        }
+
+
+        gameData.chessGame().endGame();
+
+        try {
+            gameDAO.updateGame(gameData);
+        } catch (DataAccessException e) {
+            ErrorMessage errorMessage = new ErrorMessage(ERROR, e.getMessage());
+            connections.sendError(session, errorMessage);
+            return;
+        }
+
+        var message = String.format("GAME OVER\n%s has resigned", authData.username());
+        var notification = new NotificationMessage(NOTIFICATION, message);
+        connections.sendNotification(gameID, null, notification);
     }
 
 //    private void leave(String visitorName) throws IOException {
